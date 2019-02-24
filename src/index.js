8e547c1d49e5be13f7345b50cbecf8455ec3f411
warryr <heartsyrop@gmail.com>
2019-02-24 16:27:09 +0300

    feat: add solution for the task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,35 @@
 module.exports = function getZerosCount(number, base) {
-  // your implementation
-}
+    const baseFactors = {};
+    const numberFactors = {};
+
+    //base factors
+    for (let i = 2; i <= base; i++) {
+        while ((base % i) === 0) {
+            base /= i;
+            if (baseFactors[i] === undefined){
+                baseFactors[i] = 1;
+            } else{
+                baseFactors[i]++;
+            }
+        }
+    }
+
+    //number factors
+    let  n, temp;
+    for (let key in baseFactors){
+        n = 0;
+        temp = number;
+        while(temp > 1){
+            temp = Math.floor(temp / key);
+            n += temp;
+        }
+        numberFactors[key] = n;
+    }
+
+    //count zeros
+    const arr = [];
+    for (key in baseFactors){
+        arr.push(Math.floor(numberFactors[key] / baseFactors[key]));
+    }
+    return Math.min.apply(null, arr);
+};
